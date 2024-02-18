7545075d9a30e341b3bb22e4ab149825d59a2d87
xavigu <xavigu91@gmail.com>
2024-02-18 19:04:06 +0100

    Add flowbite-svelte and dropzone component

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,9 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: [
-		'./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}', 
-		'./node_modules/flowbite-svelte/**/*.{html,js,svelte,ts}'
-	],
+		'./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
+		'./node_modules/flowbite-svelte/**/*.{html,js,svelte,ts}'],
 
   plugins: [require('flowbite/plugin')],
 
