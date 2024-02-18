7c58eb93f7b26a6dc14950bd3adb955e3bfe4f18
xavigu <xavigu91@gmail.com>
2024-02-18 18:25:42 +0100

    Add store and component for each status

--- src/components/App.svelte
+++ src/components/App.svelte
@@ -1,8 +1,26 @@
 <script>
   import { Alert } from 'flowbite-svelte';
+  import { APP_STATUS, appStatus } from '../store.ts';
+  import StepUpload from './StepUpload.svelte';
+  import StepLoading from './StepLoading.svelte';
+  import StepChat from './StepChat.svelte';
 </script>
 
-<Alert>
-  <span class="font-medium">Default alert!</span>
-  Change a few things up and try submitting again.
-</Alert>
+<!-- appStatus no tiene directamente el valor es un observable y tienes que poner el $ como para suscribirte -->
+{#if $appStatus === APP_STATUS.INIT}
+  <StepUpload />
+{:else if $appStatus === APP_STATUS.LOADING}
+  <StepLoading />
+{:else if $appStatus === APP_STATUS.ERROR}
+  <Alert>
+    <span class="font-medium">Algo malo ha pasado!</span>
+    Hay un error en la aplicación.
+  </Alert>
+{:else if $appStatus === APP_STATUS.CHAT_MODE}
+  <StepChat />
+{:else}
+  <Alert>
+    <span class="font-medium">Acción no reconocida!</span>
+    Este estado de la aplicación no esta reconocida.
+  </Alert>
+{/if}
